cbfc9b8bc9920e4dfbc4cc4bc1b791be130dee6a
orcaaarthi <rthi21292@gmail.com>
2014-08-25 15:45:40 -0700

    chap3.4

--- chp34.c
+++ chp34.c
@@ -8,12 +8,14 @@ AARTHI B,Aug 25 2014
 #include <stdio.h>
 int main (void)
 	{
-	clrscr();
+
 	int sum;
+	clrscr();
 
         //sum of 2 numbers//
 	sum = 50 + 25;
 	
 	printf ("The sum of 50 and 25 is %i\n", sum);
 	return 0;
+	
 	}
