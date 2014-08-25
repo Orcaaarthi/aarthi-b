21a75fb57e620ab8b38f971cef519f4ca0aa10d3
orcaaarthi <rthi21292@gmail.com>
2014-08-25 16:08:38 -0700

    chapter3.5

---
+++ chapter35.c
@@ -0,0 +1,23 @@
+/* chapter 3 - progarm 5*/
+
+/* progarm to displaying multiple values */
+
+AARTHI B, Aug 25,2014 
+
+
+#include <stdio.h>
+
+int main (void)
+	{
+	
+	// variable declaration
+	int value1, value2, sum;
+	value1 = 50;
+	value2 = 25;
+	
+	//sum of the 2 variables values
+	sum = value1 + value2;
+	printf ("The sum of %i and %i is %i\n", value1, value2, sum);
+	return 0;
+
+	}
