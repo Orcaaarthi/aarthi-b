26785948082a7c7b6dfe0c7fed73cbdb8c844d36
orcaaarthi <rthi21292@gmail.com>
2014-08-25 15:35:54 -0700

    chapter3.4

---
+++ chp34.c
@@ -0,0 +1,19 @@
+/*chapter 3-Program 4*/
+
+/*Program to display the variables*/
+
+AARTHI B,Aug 25 2014
+
+
+#include <stdio.h>
+int main (void)
+	{
+	clrscr();
+	int sum;
+
+        //sum of 2 numbers//
+	sum = 50 + 25;
+	
+	printf ("The sum of 50 and 25 is %i\n", sum);
+	return 0;
+	}
